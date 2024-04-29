1cde644661adad29a9cbdd5ef0b247f35984414b
Alhilal Anwar <anony7911@gmail.com>
2024-04-29 11:19:29 +0800

    cabang: update view

--- app/Livewire/Superadmin/Cabang.php
+++ app/Livewire/Superadmin/Cabang.php
@@ -2,12 +2,59 @@
 
 namespace App\Livewire\Superadmin;
 
+use Jantinnerezo\LivewireAlert\LivewireAlert;
 use Livewire\Component;
+use Livewire\WithPagination;
 
 class Cabang extends Component
 {
+    // alert
+    use LivewireAlert;
+    use WithPagination;
+    protected $paginationTheme = 'bootstrap';
+
+    public $search;
+    public $perPage = 10;
+
+    protected $cabangs;
+
+    public $sortField = 'id';
+    public $sortAsc = true;
+
+    public function sortBy($field)
+    {
+        if ($this->sortField === $field) {
+            $this->sortAsc = !$this->sortAsc;
+        } else {
+            $this->sortField = $field;
+            $this->sortAsc = true;
+        }
+    }
+    public $kode_cabang, $nama_cabang;
+
     public function render()
     {
-        return view('livewire.superadmin.cabang');
+        return view('livewire.superadmin.cabang', [
+            'title' => 'Cabang',
+            'cabangs' => \App\Models\Cabang::where('nama_cabang', 'like', '%'.$this->search.'%')
+                    ->orderBy($this->sortField, $this->sortAsc ? 'asc' : 'desc')->paginate($this->perPage)
+        ]);
+    }
+
+    public function store()
+    {
+        $this->validate([
+            'kode_cabang' => 'required',
+            'nama_cabang' => 'required',
+        ]);
+
+        \App\Models\Cabang::create([
+            'kode_cabang' => $this->kode_cabang,
+            'nama_cabang' => $this->nama_cabang,
+        ]);
+
+        $this->reset('kode_cabang', 'nama_cabang');
+        // alert
+        $this->alert('success', 'Data berhasil disimpan');
     }
 }
--- resources/views/livewire/superadmin/cabang.blade.php
+++ resources/views/livewire/superadmin/cabang.blade.php
@@ -1,3 +1,129 @@
 <div>
-    {{-- The whole world belongs to you. --}}
+    <!-- Page Heading -->
+    <div class="d-sm-flex align-items-center justify-content-between mb-2">
+        <h1 class="h3 mb-0 text-gray-800">Cabang</h1>
+        {{-- breadcrumb --}}
+        <nav aria-label="breadcrumb" class="align-items-center">
+            <ol class="breadcrumb">
+                <li class="breadcrumb-item"><a href="{{ route('superadmin.home') }}">Dashboard</a></li>
+                <li class="breadcrumb-item active" aria-current="page">Cabang</li>
+            </ol>
+        </nav>
+
+    </div>
+    <div class="row">
+
+        <div class="col-lg-12">
+            <!-- Basic Card Example -->
+            <div class="card shadow mb-4">
+                <div class="card-header py-3 d-flex justify-content-between align-items-center">
+                    <h6 class="m-0 font-weight-bold text-primary">Cabang</h6>
+                    <a href="#" class="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm" data-toggle="modal" data-target="#modalTambah"><i class="fas fa-plus fa-sm text-white-70"></i> Tambah Cabang</a>
+
+                    <!-- Modal -->
+                    <div wire:ignore.self class="modal fade" id="modalTambah" tabindex="-1" role="dialog" aria-labelledby="modalTambahLabel" aria-hidden="true">
+                        <div class="modal-dialog" role="document">
+                            <div class="modal-content">
+                                <div class="modal-header">
+                                    <h5 class="modal-title" id="modalTambahLabel">Tambah Cabang</h5>
+                                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                        <span aria-hidden="true">&times;</span>
+                                    </button>
+                                </div>
+                                <div class="modal-body">
+                                    <!-- Add your form fields here -->
+                                    <form wire:submit.prevent="store">
+                                        @csrf
+                                        <div class="form-group @error('kode_cabang') has-error @enderror">
+                                            <label for="kode_cabang">Kode Cabang</label>
+                                            <input type="text" wire:model="kode_cabang" class="form-control" id="kode_cabang" placeholder="Kode Cabang">
+                                            @error('kode_cabang')
+                                            <span class="help-block text-danger">{{ $message }}</span>
+                                            @enderror
+                                        </div>
+                                        <div class="form-group @error('nama_cabang') has-error @enderror">
+                                            <label for="nama_cabang">Nama Cabang</label>
+                                            <input type="text" wire:model="nama_cabang" class="form-control" id="nama_cabang" placeholder="Nama Cabang">
+                                            @error('nama_cabang')
+                                            <span class="help-block text-danger">{{ $message }}</span>
+                                            @enderror
+                                        </div>
+                                        <div class="modal-footer">
+                                            <button type="button" class="btn btn-secondary" data-dismiss="modal">Tutup</button>
+                                            <button type="submit" class="btn btn-primary">Simpan</button>
+                                        </div>
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="card-body">
+                    {{-- perpage, search --}}
+                    <div class="row">
+                        <div class="col-md-2">
+                            <div class="form-group
+                            @error('perPage') has-error @enderror">
+                                {{-- <label for="perPage">Per Page</label> --}}
+                                <select wire:model.live="perPage" class="form-control">
+                                    <option>5</option>
+                                    <option>10</option>
+                                    <option>15</option>
+                                    <option>20</option>
+                                </select>
+                                @error('perPage')
+                                <span class="help-block
+                                    text-danger">{{ $message }}</span>
+                                @enderror
+                            </div>
+                        </div>
+                        <div class="col-md-10">
+                            <div class="form-group @error('search') has-error @enderror">
+                                {{-- <label for="search">Search</label> --}}
+                                <input wire:model.live="search" type="text" class="form-control" placeholder="Search...">
+                                @error('search')
+                                <span class="help-block
+                                    text-danger">{{ $message }}</span>
+                                @enderror
+                            </div>
+                        </div>
+                    </div>
+                    {{-- end perpage, search --}}
+                    <div class="table-responsive">
+                        <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
+                            <thead>
+                                <tr class="text-center">
+                                    <th wire:click="sortBy('id')" width="5%">ID</th>
+                                    <th wire:click="sortBy('kode_cabang')">Kode</th>
+                                    <th wire:click="sortBy('nama_cabang')">Nama</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                @foreach ($cabangs as $cabang)
+                                <tr class="text-center">
+                                    <td>{{ $cabang->id }}</td>
+                                    <td>{{ $cabang->kode_cabang }}</td>
+                                    <td>{{ $cabang->nama_cabang }}</td>
+                                </tr>
+                                @endforeach
+                            </tbody>
+                        </table>
+                    </div>
+                    <div class="row">
+                        <div class="col-md-6">
+                            <div class="float-left text-muted">
+                                Menampilkan {{ $cabangs->firstItem() }} hingga {{ $cabangs->lastItem() }} dari
+                                {{ $cabangs->total() }} entries
+                            </div>
+                        </div>
+                        <div class="col-md-6">
+                            <div class="float-right">
+                                {{ $cabangs->links() }}
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
